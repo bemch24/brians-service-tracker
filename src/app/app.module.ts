bade031db249d9d9696473938d86ef2ad0f9ea5a
bemch24 <brian.emch@becreativesoftware.com>
2017-03-30 13:26:11 -0400

    Added firebase, hammer, material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import 'hammerjs';
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseDatabaseConfig = {
+  apiKey: "AIzaSyCcjIc0kjL8E5DhB457P8-c4oXEcjYDzDU",
+  authDomain: "servicetracker-1eb29.firebaseapp.com",
+  databaseURL: "https://servicetracker-1eb29.firebaseio.com",
+  storageBucket: "servicetracker-1eb29.appspot.com",
+  messagingSenderId: "790760958917"
+};
 
 @NgModule({
   declarations: [
     AppComponent
+
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+
+    ]),
+    AngularFireModule.initializeApp(firebaseDatabaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
